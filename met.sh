0621f0026849dcd3d413009db6dda143650a2413
mattlinux <martinspost2@gmail.com>
2022-02-13 18:50:36 +0000

    Add files via upload

---
+++ met.sh
@@ -0,0 +1,138 @@
+curl "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/"{$1}"?res=3hourly&key="{$2}"" |
+python3 -c ' #perform some preprocessing in Python, including stripping out the definitions 
+import xml.etree.ElementTree as ET
+import sys
+tree=ET.parse(sys.stdin) #parse the Met Office data from standard input
+root = tree.getroot()
+print("\n\n")
+for period in root.iter("Period"):
+     print(period.tag,period.attrib)
+     for report in period.iter("Rep"):
+        print(report.text,report.attrib)' | gawk ' #Carry out the main selection of data and format the data in gawk 
+/Period/ { 
+gsub("Z|'\''|,|}","") #strip out unwanted characters from the weather forecast date   
+split($5,date,"-") # Put the year, month and date into an array 
+printf("Date: %s",date[3]); printf("-%s",date[2]); printf("-%s\n",date[1]) #Display the date of forecast in the correct order
+}
+function weather_at_time(time) {	
+	printf("%s ",time)
+}
+function weather_report(record){
+	split(record,report," ")
+	for(i=0;i<=19;i++)
+	{
+		gsub("'\''|,","",report[i]) #strip out unwanted characters from the forecast data 
+	} 
+	printf("Likelihood of rain: %s",report[11]); printf("\045; ") #process the precipitation probability, display the the valuea % (Octal 045) 
+	printf("Wind speed: %s",report[13]); printf("mph; ") #process the wind speed
+	printf("Wind gust: %s",report[7]); printf("mph; ") #process the wind gust
+	printf("Temp: %s",report[15]); printf("C; ") #process the temperature
+	switch(report[19]) # Print substitute text for the UK Met Office weather type value
+		{
+		case "NA":
+			printf("Not available\n");
+			break
+		case "0":
+			printf("Clear night\n");
+			break
+		case "1":
+			printf("Sunny day\n");
+			break
+		case "2":
+			printf("Partly cloudy (night)\n");
+			break
+		case "3":
+			printf("Partly cloudy (day)\n");
+			break
+		case "4":
+			printf("Value not used!\n");
+			break
+		case "5":
+			printf("Mist\n");
+			break
+		case "6":
+			printf("Fog\n");
+			break
+		case "7":
+			printf("Cloudy\n");
+			break
+		case "8":
+			printf("Overcast\n");
+			break
+		case "9":
+			printf("Light rain shower (night)\n");
+			break
+		case "10":
+			printf("Light rain shower (night)\n");
+			break
+		case "11":
+			printf("Drizzle\n");
+			break
+		case "12":
+			printf("Light rain\n");
+			break
+		case "13":
+			printf("Heavy rain shower (night)\n");
+			break
+		case "14":
+			printf("Heavy rain shower (day)\n");
+			break
+		case "15":
+			printf("Heavy rain\n");
+			break
+		case "16":
+			printf("Sleet shower (night)\n");
+			break
+		case "17":
+			printf("Sleet shower (day)\n");
+			break
+		case "18":
+			printf("Sleet\n");
+			break
+		case "19":
+			printf("Hail shower (night)\n");
+			break
+		case "20":
+			printf("Hail shower (day)\n");
+			break
+		case "21":
+			printf("Hail\n");
+			break
+		case "22":
+			printf("Light snow shower (night)\n");
+			break
+		case "23":
+			printf("show shower (day)\n");
+			break
+		case "24":
+			printf("Light snow\n");
+			break
+		case "25":
+			printf("Heavy snow shower (night)\n");
+			break
+		case "26":
+			printf("Heavy snow shower (day)\n");
+			break
+		case "27":
+			printf("Heavy snow\n");
+			break
+		case "28":
+			printf("Thunder shower (night)\n");
+			break
+		case "29":
+			printf("Thunder shower (day)\n");
+			break
+		case "30":
+			printf("Thunder\n");
+			break
+		}
+}
+/^0/ 	{weather_at_time("00:00hrs "); weather_report($0)}
+/^180/ 	{weather_at_time("03:00hrs "); weather_report($0)}
+/^360/ 	{weather_at_time("06:00hrs "); weather_report($0)}
+/^540/ 	{weather_at_time("09:00hrs "); weather_report($0)}
+/^720/ 	{weather_at_time("12:00hrs "); weather_report($0)}
+/^900/ 	{weather_at_time("15:00hrs "); weather_report($0)}
+/^1080/	{weather_at_time("18:00hrs "); weather_report($0)}
+/^1260/ {weather_at_time("21:00hrs "); weather_report($0)}
+'
